e0776ef14ce56d6fef292649b3163775dc802277
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2018-11-24 22:38:47 +0800

    more info in README.md

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link type="text/css" rel="stylesheet" href="jesp.css" />
-<title>Join, Eval, and Sort Tables</title>
+<title>Join, Evaluate, and Sort Tables</title>
 </head>
 <body>
 
