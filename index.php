4520ebbc4f4460173af302a155f283c636f0ed40
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2019-12-31 10:24:31 +0800

    doctype & xmlns minor fixes

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link type="text/css" rel="stylesheet" href="jesp.css" />
+
+
+
 <title>Join, Evaluate, and Sort Tables</title>
+
 </head>
 <body>
-
-</p>
 
 <?php
 # https://installlion.com/ubuntu/xenial/universe/p/php-symfony-expression-language/install/index.html
@@ -172,17 +177,20 @@ echo "$err_log";
 
 ?>
 
+</body>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jq-3.3.1/dt-1.10.18/fh-3.1.4/datatables.min.css"/>
 <!--
 https://datatables.net/download/
 bootstrap3, jQuery3, Bootstrap3, DataTables, FixedHeaders
 Minify+Concatenate
 -->
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jq-3.3.1/dt-1.10.18/fh-3.1.4/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-3.3.1/dt-1.10.18/fh-3.1.4/datatables.min.js"></script>
-  <script type="text/javascript" src="https://cdn.plot.ly/plotly-basic-latest.min.js" ></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.19.1/URI.js" ></script>
+<script type="text/javascript" src="https://cdn.plot.ly/plotly-basic-latest.min.js" ></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.19.1/URI.js" ></script>
+<script type="text/javascript" src="jesp.js"> </script>
 
-  <script type="text/javascript" src="jesp.js"> </script>
-</body>
+<link type="text/css" rel="stylesheet" href="jesp.css" />
+
 </html>
